848f9ebf7d2df1e8227b8e6c6127080e6ddc796a
Niels Meima <nielsmeima@Nielss-MacBook-Pro.local>
2019-12-01 16:49:04 +0100

    tutorial finished

--- consumer/src/main/java/App.java
+++ consumer/src/main/java/App.java
@@ -1,27 +1,55 @@
 import javax.jms.*;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
-/**
- * Hello world!
- */
-public class App {
+public class App
+{
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)
+    {
+        Thread t1 = new Thread(new Receiver("Thread #1"));
+        Thread t2 = new Thread(new Receiver("Thread #2"));
+
+        t1.start();
+        t2.start();
+
+        while (true) {}
+    }
+}
+
+
+class Receiver implements Runnable
+{
+    String topic;
+
+    public Receiver(String topic)
+    {
+        this.topic = topic;
+    }
+
+
+    public void run()
+    {
         receiveMessage();
     }
 
-    public static void receiveMessage() throws NamingException {
-        InitialContext namingContext = new InitialContext();
+    private void receiveMessage()
+    {
+        try
+        {
+            InitialContext namingContext = new InitialContext();
 
-        ConnectionFactory connectionFactory = (ConnectionFactory) namingContext.lookup("ConnectionFactory");
+            ConnectionFactory connectionFactory = (ConnectionFactory) namingContext.lookup("ConnectionFactory");
 
-        Queue testQueue = (Queue) namingContext.lookup("queues/TutorialQueue");
+//          Topic destination = (Topic) namingContext.lookup("topics/TutorialTopic");
+            Queue destination = (Queue) namingContext.lookup("queues/TutorialQueue");
 
-        try (JMSContext context=connectionFactory.createContext("admin", "admin")) {
-            JMSConsumer consumer = (JMSConsumer) context.createConsumer(testQueue);
+            try (JMSContext context = connectionFactory.createContext("admin", "admin", JMSContext.AUTO_ACKNOWLEDGE))
+            {
+                JMSConsumer consumer = context.createConsumer(destination);
+//                JMSConsumer consumer = context.createConsumer(destination, String.format("topic = '%s'", this.topic));
 
-            consumeAsync(consumer);
+                consumeAsync(consumer);
+            }
         }
         catch (Exception e)
         {
@@ -29,30 +57,41 @@ public class App {
         }
     }
 
-    private static void consumeSync(JMSConsumer consumer) {
+    private void consumeSync(JMSConsumer consumer)
+    {
         String body = consumer.receiveBody(String.class);
-        System.out.println(body);
+        System.out.println(String.format("[%s] %s", this.topic, body));
     }
 
-    private static void consumeAsync(JMSConsumer consumer) {
-        MessageListener messageListener = new TutorialMessageListener();
+    private void consumeAsync(JMSConsumer consumer)
+    {
+        MessageListener messageListener = new TutorialMessageListener(this.topic);
         consumer.setMessageListener(messageListener);
 
         while(true) {}
     }
-
 }
 
 
-class TutorialMessageListener implements MessageListener {
+class TutorialMessageListener implements MessageListener
+{
+    String topic;
 
-    public void onMessage(Message message) {
+    public TutorialMessageListener(String topic)
+    {
+        this.topic = topic;
+    }
+
+    public void onMessage(Message message)
+    {
         TextMessage textMessage = (TextMessage) message;
-        try {
+        try
+        {
             String text = textMessage.getText();
-            System.out.println(text);
+            System.out.println(String.format("[%s] %s", this.topic, text));
         }
-        catch (Exception e) {
+        catch (Exception e)
+        {
             System.out.println(e);
         }
 
--- producer/src/main/java/App.java
+++ producer/src/main/java/App.java
@@ -1,26 +1,58 @@
 import javax.jms.*;
+import javax.jms.Queue;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
-/**
- * Hello world!
- */
 public class App {
 
-    public static void main(String[] args) throws Exception {
-        sendMessageNew("Hello!");
+    public static void main(String[] args) throws Exception
+    {
+        Random rand = new Random();
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+
+        ArrayList<String> topics = new ArrayList<>();
+        topics.add("Channels");
+        topics.add("Routing");
+        topics.add("JMS concepts");
+        topics.add("Complex Event Processing");
+
+        while (true)
+        {
+            Date date = Calendar.getInstance().getTime();
+            String dateString = dateFormat.format(date);
+
+            int randomIndex = rand.nextInt(topics.size());
+            String topic = topics.get(randomIndex);
+
+            System.out.printf("[%s] %s\n", topic, dateString);
+
+            sendMessage(topic, dateString);
+            Thread.sleep(1000);
+        }
+
     }
 
-    public static void sendMessageNew(String body) throws NamingException {
+    public static void sendMessage(String topic, String body) throws NamingException
+    {
         InitialContext namingContext = new InitialContext();
 
         ConnectionFactory connectionFactory = (ConnectionFactory) namingContext.lookup("ConnectionFactory");
 
-        Queue testQueue = (Queue) namingContext.lookup("queues/TutorialQueue");
+//        Topic destination = (Topic) namingContext.lookup("topics/TutorialTopic");
+        Queue destination = (Queue) namingContext.lookup("queues/TutorialQueue");
 
-        try (JMSContext context=connectionFactory.createContext("admin", "admin")) {
+        try (JMSContext context = connectionFactory.createContext("admin", "admin", JMSContext.AUTO_ACKNOWLEDGE))
+        {
 
-            context.createProducer().send(testQueue, body);
+            context
+                .createProducer()
+                .setDeliveryMode(DeliveryMode.PERSISTENT)
+                .setProperty("topic", topic)
+                .send(destination, body);
         }
         catch (Exception e)
         {
